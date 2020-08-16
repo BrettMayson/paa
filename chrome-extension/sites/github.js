89206cf8bbe34bf210102c46f4f054f8583e8fe5
jonpas <jonpas33@gmail.com>
2020-08-16 17:55:39 +0200

    Add support for diff pages

--- chrome-extension/sites/github.js
+++ chrome-extension/sites/github.js
@@ -1,16 +1,46 @@
 function pathChanged() {
-    if (!window.location.pathname.endsWith(".paa")) return;
+    // File view page
+    if (window.location.pathname.endsWith(".paa")) {
+        let interval = setInterval(function() {
+            try {
+                let wrapper = document.getElementsByClassName("blob-wrapper")[0].firstChild.nextSibling.firstChild.nextSibling;
+                wrapper.textContent = "Converting PAA...";
+                clearInterval(interval);
+                let image = document.createElement("img");
+                chrome.runtime.sendMessage({contentScriptQuery: "fetch_blob", src: "https://github.com" + window.location.pathname + "?raw=true"}, response => {
+                    image.src = response;
+                    wrapper.replaceWith(image);
+                });
+            } catch {}
+        }, 10);
+    }
+
+    // Diff page
+    let diffs = document.querySelectorAll("div[data-file-type='.paa'][class~='file']");
     let interval = setInterval(function() {
-        try {
-            let wrapper = document.getElementsByClassName("blob-wrapper")[0].firstChild.nextSibling.firstChild.nextSibling;
-            wrapper.textContent = "Converting PAA...";
-            clearInterval(interval);
-            let image = document.createElement("img");
-            chrome.runtime.sendMessage({contentScriptQuery: "fetch_blob", src: "https://github.com" + window.location.pathname + "?raw=true"}, response => {
-                image.src = response;
-                wrapper.replaceWith(image);
-            });
-        } catch {}
+        for (diff of diffs) {
+            try {
+                let wrapper = diff.firstElementChild.nextElementSibling.firstElementChild;
+                wrapper.textContent = "Converting PAA...";
+                clearInterval(interval);
+
+                // Put into image sizing div like on file view page to center
+                let divimage = document.createElement("div");
+                divimage.setAttribute("itemprop", "text");
+                divimage.setAttribute("class", "Box-body p-0 blob-wrapper data type-text gist-border-0");
+                let divcenter = document.createElement("div");
+                divcenter.setAttribute("class", "text-center p-3");
+                divimage.appendChild(divcenter);
+                let image = document.createElement("img");
+                divcenter.appendChild(image);
+
+                let rawlink = diff.querySelectorAll("a")[1].href; // "View file" button link
+                chrome.runtime.sendMessage({contentScriptQuery: "fetch_blob", src: rawlink + "?raw=true"}, response => {
+                    image.src = response;
+                    wrapper.replaceWith(divimage);
+                });
+            } catch {}
+        }
     }, 10);
 }
 
